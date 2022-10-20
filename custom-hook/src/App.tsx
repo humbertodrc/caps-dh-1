72b7cb87d2b2e6c5efb66365782c273acfe7f4f0
danieldamian09 <humbertorivero9@gmail.com>
2022-10-20 20:32:08 -0300

    Add project custom hook

--- custom-hook/src/App.tsx
+++ custom-hook/src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Character from './components/Character';
+import useCounter from "./hooks/useCounter";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { count, increment, decrement } = useCounter();
 
   return (
-    <div className="App">
+    <article className="App">
+      <Character count={count} />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Contador = {count}</h2>
+        <span>
+          <button onClick={increment}>Incrementar</button>
+          <button onClick={decrement}>Decrementar</button>
+        </span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    </article>
+  );
+};
 
-export default App
+export default App;
